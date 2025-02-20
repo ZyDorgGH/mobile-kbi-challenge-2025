e9b34af97f9f4f98f85a57edbdf1c7d7da2515cb
actiangent <hisyam.fausta@gmail.com>
2025-02-20 07:45:06 +0700

    Add job bookmark repository

--- app/src/main/java/id/kitabantu/di/DataModule.kt
+++ app/src/main/java/id/kitabantu/di/DataModule.kt
@@ -5,7 +5,9 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import id.kitabantu.data.remote.RemoteJobDataSource
+import id.kitabantu.data.repository.DefaultJobBookmarkRepository
 import id.kitabantu.data.repository.DefaultJobRepository
+import id.kitabantu.data.repository.JobBookmarkRepository
 import id.kitabantu.data.repository.JobRepository
 import javax.inject.Singleton
 
@@ -17,5 +19,11 @@ object DataModule {
     @Provides
     fun provideJobRepository(remoteJobDataSource: RemoteJobDataSource): JobRepository {
         return DefaultJobRepository(remoteJobDataSource)
+    }
+
+    @Singleton
+    @Provides
+    fun provideJobBookmarkRepository(): JobBookmarkRepository {
+        return DefaultJobBookmarkRepository()
     }
 }
