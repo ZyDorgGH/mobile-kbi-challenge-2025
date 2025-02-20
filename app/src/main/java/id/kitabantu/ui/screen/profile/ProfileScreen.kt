8352a246aa8ffb1c9954bc28750197cc593adadb
Yonatan Arif <90676226+yonatan2809@users.noreply.github.com>
2025-02-21 02:32:58 +0700

    Update ProfileScreen.kt

--- app/src/main/java/id/kitabantu/ui/screen/profile/ProfileScreen.kt
+++ app/src/main/java/id/kitabantu/ui/screen/profile/ProfileScreen.kt
@@ -1,23 +1,73 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package id.kitabantu.ui.screen.profile
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import id.kitabantu.R
 
 @Composable
 fun ProfileScreen(
-    modifier: Modifier = Modifier,
     navigateToBookmark: () -> Unit
-){
-    Column {
-        Text(text = "Profile Screen")
-        Button(onClick = {
-            navigateToBookmark()
-        }) {
-            Text(text = "Bookmark Screen")
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Profile") }
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(30.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.profile),
+                contentDescription = "Profile Picture",
+                modifier = Modifier
+                    .size(130.dp)
+                    .clip(CircleShape)
+                    .background(Color.Gray)
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Text(
+                text = "Hisyam Fausta",
+                style = MaterialTheme.typography.headlineSmall
+            )
+
+            Text(
+                text = "hisyamfausta@gmail.com",
+                style = MaterialTheme.typography.bodyMedium,
+                color = Color.Gray
+            )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Divider()
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+
+            Column {
+                Button(onClick = { navigateToBookmark() }) {
+                    Text(text = "Bookmark Screen")
+                }
+            }
         }
     }
-
 }
